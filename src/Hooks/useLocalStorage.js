3799538f1e31f92cd23dfc7a47f4630fba48d43c
jaredhall18 <jaredhall18@gmail.com>
2021-10-27 13:57:02 -0600

    created setDarkMode hook

--- src/Hooks/useLocalStorage.js
+++ src/Hooks/useLocalStorage.js
@@ -1,8 +1,12 @@
 import React, {useState, useEffect} from 'react'
 
 const useLocalStorage =(key, initialValue) => {
+    // To retrieve an item from localStorage, call localStorage.getItem('itemName')
+// If that item doesn't exist, it will return undefined
     const [storedValue, setStoredValue]= useState(()=>{
+            // Get from local storage by key
         if(localStorage.getItem(key)) {
+            // Parse and return stored json or, if undefined, return initialValue
             return JSON.parse(localStorage.getItem(key));
         }else {
             localStorage.setItem(key, JSON.stringify(initialValue));
@@ -11,7 +15,9 @@ const useLocalStorage =(key, initialValue) => {
     })
 
     const setValue = (value) => {
+        // Save to local storage
         localStorage.setItem(key, JSON.stringify(value));
+        // Save state
         setStoredValue(value);
     }
 
